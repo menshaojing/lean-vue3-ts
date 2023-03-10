559e1dd8ed79fba40a5f1a765dc5fbaacaa5dbf5
时空留痕 <994271261@qq.com>
2023-03-10 17:26:44 +0800

    集成vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-createApp(App).mount('#app')
-const title =document.getElementsByTagName("title")[0];
-title.text=import.meta.env.VITE_APP_TITLE;
-console.log(import.meta.env)
-console.log(title)
-const h1= document.createElement("h1");
-h1.innerHTML=import.meta.env.APP_NAME;
-document.body.appendChild(h1);
+import router from './router'
+const app=createApp(App);
+//注册路由
+app.use(router)
+
+app.mount('#app')
+
+
+//const title =document.getElementsByTagName("title")[0];
+//title.text=import.meta.env.VITE_APP_TITLE;
+//console.log(import.meta.env)
+//console.log(title)
+//const h1= document.createElement("h1");
+//h1.innerHTML=import.meta.env.APP_NAME;
+//document.body.appendChild(h1);
 
 
